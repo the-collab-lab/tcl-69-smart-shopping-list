6a89d80e790fcf21f503f05112bd81206491f1c2
Jessica Hsiang <hsiang.jessica@gmail.com>
2024-02-13 12:43:37 -0800

    Replace console.log in addItem with Firebase function addDoc. Setup prelim try/catch

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,5 +1,6 @@
 import {
 	arrayUnion,
+	addDoc,
 	getDoc,
 	setDoc,
 	collection,
@@ -165,15 +166,21 @@ export async function addItem(listPath, { itemName, daysUntilNextPurchase }) {
 	const listCollectionRef = collection(db, listPath, 'items');
 	// TODO: Replace this call to console.log with the appropriate
 	// Firebase function, so this information is sent to your database!
-	return console.log(listCollectionRef, {
-		dateCreated: new Date(),
-		// NOTE: This is null because the item has just been created.
-		// We'll use updateItem to put a Date here when the item is purchased!
-		dateLastPurchased: null,
-		dateNextPurchased: getFutureDate(daysUntilNextPurchase),
-		name: itemName,
-		totalPurchases: 0,
-	});
+	try {
+		const newDoc = await addDoc(listCollectionRef, {
+			dateCreated: new Date(),
+			// NOTE: This is null because the item has just been created.
+			// We'll use updateItem to put a Date here when the item is purchased!
+			dateLastPurchased: null,
+			dateNextPurchased: getFutureDate(daysUntilNextPurchase),
+			name: itemName,
+			totalPurchases: 0,
+		});
+		console.log('success', newDoc);
+		return newDoc;
+	} catch (err) {
+		console.error('Error adding new item:', err);
+	}
 }
 
 export async function updateItem() {
