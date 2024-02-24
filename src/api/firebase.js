7cc8e10c87057b835a7ef83900ce4a815a9ad76d
krsnamara <martinj.fitzpatrick@gmail.com>
2024-02-24 13:31:26 -0500

    Add error message to trigger return when recipient email does not exisit

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -147,8 +147,9 @@ export async function shareList(listPath, currentUserId, recipientEmail) {
 	const recipientDoc = await getDoc(doc(usersCollectionRef, recipientEmail));
 	// If the recipient user doesn't exist, we can't share the list.
 	if (!recipientDoc.exists()) {
-		return;
+		return 'The user you are trying to invite does not exist.';
 	}
+
 	// Add the list to the recipient user's sharedLists array.
 	const listDocumentRef = doc(db, listPath);
 	const userDocumentRef = doc(db, 'users', recipientEmail);
