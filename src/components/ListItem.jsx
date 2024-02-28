db4d5e9a92087506f8665866dbc2dbfe387f33a9
Jessica Hsiang <hsiang.jessica@gmail.com>
2024-02-28 13:28:51 -0800

    Replace <li> with checkbox input with label. Add handleCheck to update item with Firebase api call. Setup useEffect for checkbox auto uncheck after 24 hours

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,6 +2,68 @@ import './ListItem.css';
 import { updateItem } from '../api';
 import { useState, useEffect } from 'react';
 
-export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+export function ListItem({ item, listPath }) {
+	const { id, name, dateLastPurchased } = item;
+
+	const currentDate = new Date();
+	const lastPurchasedDate = dateLastPurchased
+		? new Date(dateLastPurchased.seconds * 1000)
+		: 0;
+	const timeDiff = currentDate - lastPurchasedDate; //milliseconds
+	const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+	const [isChecked, setIsChecked] = useState(timeDiff < oneDayInMilliseconds);
+
+	useEffect(() => {
+		let timeoutId;
+		if (isChecked && timeDiff < oneDayInMilliseconds) {
+			const timeLeft = oneDayInMilliseconds - timeDiff;
+			timeoutId = setTimeout(() => {
+				setIsChecked(false);
+			}, timeLeft);
+		}
+
+		return () => clearTimeout(timeoutId);
+	}, [isChecked, timeDiff, oneDayInMilliseconds]);
+
+	async function handleCheck(e) {
+		const timeLeftInMilliseconds = oneDayInMilliseconds - timeDiff;
+		if (isChecked && timeLeftInMilliseconds > 0) {
+			const timeLeftInHours = Math.floor(
+				timeLeftInMilliseconds / (1000 * 60 * 60),
+			);
+			const timeLeftInMinutes = Math.floor(
+				(timeLeftInMilliseconds / (1000 * 60)) % 60,
+			);
+			const alertMessage =
+				timeLeftInHours <= 0
+					? `You can set this item to be purchased again in ${timeLeftInMinutes} minutes`
+					: `You can set this item to be purchased again in ${timeLeftInHours} hours and ${timeLeftInMinutes} minutes`;
+			alert(alertMessage);
+		} else {
+			setIsChecked(e.target.checked);
+			if (e.target.checked) {
+				const result = await updateItem(listPath, id);
+				if (result.success) {
+					alert('Item successfully updated');
+				} else {
+					alert('Error: item not updated');
+				}
+			}
+		}
+	}
+
+	return (
+		<div>
+			<label htmlFor={name} className="ListItem">
+				<input
+					type="checkbox"
+					id={name}
+					onChange={handleCheck}
+					name={name}
+					checked={isChecked}
+				/>
+				{name}
+			</label>
+		</div>
+	);
 }
