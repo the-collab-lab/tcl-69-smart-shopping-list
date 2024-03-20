76abb87754d976624ea8bbfbf3beb73a3a8cefe7
piecanoe <karenkaydu@gmail.com>
2024-03-20 16:46:47 -0700

    Add Footer and attribution to footer

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -37,6 +37,56 @@ export function Layout() {
 						</NavLink>
 					</div>
 				</nav>
+				<footer className="Layout-footer">
+					<p className="Layout-footer-attribution">
+						Created by{' '}
+						<a
+							className="Layout-footer-link"
+							href="#"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Amalya
+						</a>
+						,{' '}
+						<a
+							className="Layout-footer-link"
+							href="#"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Jessica
+						</a>
+						,{' '}
+						<a
+							className="Layout-footer-link"
+							href="#"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Karen
+						</a>
+						, &{' '}
+						<a
+							className="Layout-footer-link"
+							href="#"
+							rel="noreferrer"
+							target="_blank"
+						>
+							Marty
+						</a>{' '}
+						(aka the Box Makers) in partnership with{' '}
+						<a
+							className="Layout-footer-link"
+							href="https://the-collab-lab.codes/"
+							rel="noreferrer"
+							target="_blank"
+						>
+							The Collab Lab
+						</a>
+						.
+					</p>
+				</footer>
 			</div>
 		</>
 	);
