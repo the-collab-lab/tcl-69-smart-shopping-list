d2f678a21c670b28e776b2fbbcb0a1ffcb574468
Amalya Megerman <amalyamegerman@gmail.com>
2024-03-22 12:58:36 -0400

    add basic layout elements

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,25 @@
 import './Home.css';
+import { useAuth, SignInButton, SignOutButton } from '../api/useAuth.jsx';
+import { auth } from '../api/config.js';
 
 export function Home() {
+	const { user } = useAuth();
 	return (
-		<div className="Home">
-			<h2>Welcome to Box Makers!</h2>
-			<img src="img/checkbox.png" alt="black checkbox with green check" />
-		</div>
+		<>
+			<div className="Home">
+				<header className="Home-header">
+					<h2>Welcome to Box Makers!</h2>
+					<img src="img/checkbox.png" alt="black checkbox with green check" />
+					{!!user ? (
+						<div>
+							<span>Signed in as {auth.currentUser.displayName}</span> (
+							<SignOutButton />)
+						</div>
+					) : (
+						<SignInButton />
+					)}
+				</header>
+			</div>
+		</>
 	);
 }
