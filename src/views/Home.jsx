12391c6fa17320a744c1b6ead380d51acbc39615
Amalya Megerman <amalyamegerman@gmail.com>
2024-03-22 11:50:21 -0400

    add checkbox image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,8 @@ import './Home.css';
 export function Home() {
 	return (
 		<div className="Home">
-			<p>Welcome to Box Makers!</p>
+			<h2>Welcome to Box Makers!</h2>
+			<img src="img/checkbox.png" alt="black checkbox with green check" />
 		</div>
 	);
 }
