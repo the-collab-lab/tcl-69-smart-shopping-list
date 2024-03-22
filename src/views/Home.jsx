3310c812e289f4cce05f83582a76d1e740268662
Amalya Megerman <amalyamegerman@gmail.com>
2024-03-22 13:26:01 -0400

    add link to about page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css';
 import { useAuth, SignInButton, SignOutButton } from '../api/useAuth.jsx';
 import { auth } from '../api/config.js';
+import { Link } from 'react-router-dom';
 
 export function Home() {
 	const { user } = useAuth();
@@ -9,7 +10,14 @@ export function Home() {
 			<div className="Home">
 				<header className="Home-header">
 					<h2>Welcome to Box Makers!</h2>
-					<img src="img/checkbox.png" alt="black checkbox with green check" />
+					<img
+						className="checkbox"
+						src="img/checkbox.png"
+						alt="black checkbox with green check"
+					/>
+					<h3>
+						New to the app? Click <Link to="/about">here</Link> to learn more!
+					</h3>
 					{!!user ? (
 						<div>
 							<span>Signed in as {auth.currentUser.displayName}</span> (
