726185e7986fcdf5ee343c0086f3b3f3aa7bf4cf
krsnamara <martinj.fitzpatrick@gmail.com>
2024-04-01 23:56:07 -0400

    Renames classes for newly created css file

--- src/components/SortedItemsMap.jsx
+++ src/components/SortedItemsMap.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getSortedItems, getGroupedItems } from '../utils';
 import { ListItem } from './ListItem';
+import './SortedItemsMap.css';
 
 export function SortedItemsMap({ filteredDataResult, listPath }) {
 	const sortedDataResult = getSortedItems(filteredDataResult);
@@ -9,27 +10,27 @@ export function SortedItemsMap({ filteredDataResult, listPath }) {
 		getGroupedItems(sortedDataResult);
 	return (
 		<>
-			<h4 className="List-section-header">Overdue</h4>
+			<h4 className="SortedItems-section-header">Overdue</h4>
 			{overdue.map((item) => (
 				<ListItem key={item.id} item={item} listPath={listPath} />
 			))}
-			<hr className="list-section-divider" />
-			<h4 className="List-section-header">Soon</h4>
+			<hr className="SortedItems-section-divider" />
+			<h4 className="SortedItems-section-header">Soon</h4>
 			{buySoon.map((item) => (
 				<ListItem key={item.id} item={item} listPath={listPath} />
 			))}
-			<hr className="list-section-divider" />
-			<h4 className="List-section-header">Kind of soon</h4>
+			<hr className="SortedItems-section-divider" />
+			<h4 className="SortedItems-section-header">Kind of soon</h4>
 			{buyKindOfSoon.map((item) => (
 				<ListItem key={item.id} item={item} listPath={listPath} />
 			))}
-			<hr className="list-section-divider" />
-			<h4 className="List-section-header">Not soon</h4>
+			<hr className="SortedItems-section-divider" />
+			<h4 className="SortedItems-section-header">Not soon</h4>
 			{buyNotSoon.map((item) => (
 				<ListItem key={item.id} item={item} listPath={listPath} />
 			))}
-			<hr className="list-section-divider" />
-			<h4 className="List-section-header">Inactive</h4>
+			<hr className="SortedItems-section-divider" />
+			<h4 className="SortedItems-section-header">Inactive</h4>
 			{inactive.map((item) => (
 				<ListItem key={item.id} item={item} listPath={listPath} />
 			))}
